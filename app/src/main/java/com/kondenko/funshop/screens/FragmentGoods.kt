eb2f6a18794ac7ac1a4f23c98ec14cf03dc1c427
Vladimir Kondenko <kondenko2011@gmail.com>
2019-06-30 14:07:14 +0300

    Use a dedicated "Buy" button for each good

--- app/src/main/java/com/kondenko/funshop/screens/FragmentGoods.kt
+++ app/src/main/java/com/kondenko/funshop/screens/FragmentGoods.kt
@@ -1,11 +1,8 @@
 package com.kondenko.funshop.screens
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.kondenko.funshop.R
 import com.kondenko.funshop.entities.Good
 import com.kondenko.funshop.screens.backend.AdapterGoods
 import com.kondenko.funshop.screens.flux.Action
@@ -16,9 +13,6 @@ import com.kondenko.funshop.utils.subscribe
 abstract class FragmentGoods : Fragment() {
 
     abstract var adapterGoods: AdapterGoods
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
-        inflater.inflate(R.layout.fragment_backend, container, false)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
--- app/src/main/java/com/kondenko/funshop/screens/main/ActivityMain.kt
+++ app/src/main/java/com/kondenko/funshop/screens/main/ActivityMain.kt
@@ -1,16 +1,12 @@
 package com.kondenko.funshop.screens.main
 
 import android.os.Bundle
-import android.view.View
-import android.view.animation.AccelerateInterpolator
-import android.view.animation.DecelerateInterpolator
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.kondenko.funshop.R
 import com.kondenko.funshop.screens.backend.FragmentBackend
 import com.kondenko.funshop.screens.store.FragmentStore
 import com.kondenko.funshop.screens.viewmodel.GoodsViewModelImpl
-import com.kondenko.funshop.utils.animate
 import kotlinx.android.synthetic.main.activity_main.*
 import org.koin.android.viewmodel.ext.android.viewModel
 
@@ -49,39 +45,10 @@ class ActivityMain : AppCompatActivity() {
 
     private fun selectStore() {
         setFragment(fragmentStore, tagStore)
-        fab_action.apply {
-            setImageResource(R.drawable.ic_add_to_cart)
-            playPopAnimation()
-        }
     }
 
     private fun selectBackend() {
         setFragment(fragmentBackend, tagBackend)
-        fab_action.apply {
-            setImageResource(R.drawable.ic_add)
-            playPopAnimation()
-        }
-    }
-
-    private fun View.playPopAnimation() {
-        val originalScaleX = scaleX
-        val originalScaleY = scaleY
-        val factor = 0.85f
-        val animDuration = 150L
-        animate {
-            duration = animDuration
-            interpolator = AccelerateInterpolator()
-            scaleX(originalScaleX * factor)
-            scaleY(originalScaleY * factor)
-            withEndAction {
-                animate {
-                    duration = animDuration
-                    interpolator = DecelerateInterpolator()
-                    scaleX(originalScaleX)
-                    scaleY(originalScaleY)
-                }
-            }
-        }
     }
 
     private fun setFragment(fragment: Fragment, tag: String) {
