a021aed611af176fb9db0148afc0dba158f5b64a
Vladimir Kondenko <kondenko2011@gmail.com>
2019-07-03 14:51:44 +0300

    Optimize GetGoods and BuyGood logic

--- app/src/main/java/com/kondenko/funshop/domain/BuyGood.kt
+++ app/src/main/java/com/kondenko/funshop/domain/BuyGood.kt
@@ -3,23 +3,21 @@ package com.kondenko.funshop.domain
 import com.kondenko.funshop.data.GoodsRepository
 import com.kondenko.funshop.entities.Good
 import com.kondenko.funshop.utils.Schedulers
-import io.reactivex.Single
+import io.reactivex.Completable
 import java.util.concurrent.TimeUnit
 
 class BuyGood(private val goodsRepository: GoodsRepository, private val schedulers: Schedulers) :
-    UseCase.Fetch<Good, Good>(schedulers) {
+    UseCase.Do<Good>(schedulers) {
 
     private val delay: Long = 3
 
     override fun build(params: Good) =
         if (params.quantity > 0) {
-            Single.timer(delay, TimeUnit.SECONDS, schedulers.worker).flatMap {
-                goodsRepository
-                    .update(params.copy(quantity = params.quantity - 1))
-                    .andThen(Single.just(params.copy(metadata = params.metadata?.copy(isBeingProcessed = false))))
-            }
+            val delay = Completable.timer(delay, TimeUnit.SECONDS, schedulers.worker)
+            val purchase = goodsRepository.update(params.copy(quantity = params.quantity - 1))
+            Completable.concatArray(delay, purchase)
         } else {
-            Single.error(IllegalArgumentException("Can't buy a good with quantity = 0: $params"))
+            Completable.error(IllegalArgumentException("Can't buy a good with quantity = 0: $params"))
         }
 
 }
--- app/src/main/java/com/kondenko/funshop/domain/GetGoods.kt
+++ app/src/main/java/com/kondenko/funshop/domain/GetGoods.kt
@@ -3,16 +3,15 @@ package com.kondenko.funshop.domain
 import com.kondenko.funshop.data.GoodsRepository
 import com.kondenko.funshop.entities.Good
 import com.kondenko.funshop.utils.Schedulers
-import io.reactivex.Completable
+import io.reactivex.Observable
 import java.util.concurrent.TimeUnit
 
 class GetGoods(private val goodsRepository: GoodsRepository, private val schedulers: Schedulers) :
-        UseCase.Subscribe<Nothing?, List<Good>>(schedulers) {
+    UseCase.Subscribe<Nothing?, List<Good>>(schedulers) {
 
     private val delaySec: Long = 5
 
-    override fun build(params: Nothing?) = Completable.timer(delaySec, TimeUnit.SECONDS, schedulers.ui).andThen(
-            goodsRepository.getGoods()
-    )
+    override fun build(params: Nothing?): Observable<List<Good>> =
+        goodsRepository.getGoods().delay(delaySec, TimeUnit.SECONDS, schedulers.ui)
 
 }
