5056648556f16e2f28183c9d1739e931b27cf572
Vladimir Kondenko <kondenko2011@gmail.com>
2019-07-05 16:13:06 +0300

    Move the 5 seconds delay from GetGoods to AddOrUpdateGood

--- app/src/main/java/com/kondenko/funshop/domain/AddOrUpdateGood.kt
+++ app/src/main/java/com/kondenko/funshop/domain/AddOrUpdateGood.kt
@@ -3,10 +3,14 @@ package com.kondenko.funshop.domain
 import com.kondenko.funshop.data.GoodsRepository
 import com.kondenko.funshop.entities.Good
 import com.kondenko.funshop.utils.Schedulers
+import java.util.concurrent.TimeUnit
 
-class AddOrUpdateGood(private val goodsRepository: GoodsRepository, schedulers: Schedulers) :
+class AddOrUpdateGood(private val goodsRepository: GoodsRepository, private val schedulers: Schedulers) :
     UseCase.Do<Good>(schedulers) {
 
+    private val delaySec: Long = 5
+
     override fun build(params: Good) = goodsRepository.addOrUpdate(params)
+        .delay(delaySec, TimeUnit.SECONDS, schedulers.ui)
 
 }
--- app/src/main/java/com/kondenko/funshop/domain/GetGoods.kt
+++ app/src/main/java/com/kondenko/funshop/domain/GetGoods.kt
@@ -4,14 +4,11 @@ import com.kondenko.funshop.data.GoodsRepository
 import com.kondenko.funshop.entities.Good
 import com.kondenko.funshop.utils.Schedulers
 import io.reactivex.Observable
-import java.util.concurrent.TimeUnit
 
-class GetGoods(private val goodsRepository: GoodsRepository, private val schedulers: Schedulers) :
+class GetGoods(private val goodsRepository: GoodsRepository, schedulers: Schedulers) :
     UseCase.Subscribe<Nothing?, List<Good>>(schedulers) {
 
-    private val delaySec: Long = 5
-
     override fun build(params: Nothing?): Observable<List<Good>> =
-        goodsRepository.getGoods().delay(delaySec, TimeUnit.SECONDS, schedulers.ui)
+        goodsRepository.getGoods()
 
 }
