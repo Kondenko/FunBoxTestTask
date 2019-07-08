199b6cc14ba804b68c83e7b980d520729bf8d054
Vladimir Kondenko <kondenko2011@gmail.com>
2019-07-08 19:36:55 +0300

    Remove an unused UseCase subclass

--- app/src/main/java/com/kondenko/funshop/domain/UseCase.kt
+++ app/src/main/java/com/kondenko/funshop/domain/UseCase.kt
@@ -3,22 +3,12 @@ package com.kondenko.funshop.domain
 import com.kondenko.funshop.utils.Schedulers
 import io.reactivex.Completable
 import io.reactivex.Observable
-import io.reactivex.Single
 
 sealed class UseCase<T, R, C> {
 
     protected abstract fun build(params: T): C
 
     abstract operator fun invoke(params: T): C
-
-    abstract class Fetch<T, R>(private val schedulers: Schedulers) : UseCase<T, R, Single<R>>() {
-
-        override fun invoke(params: T): Single<R> =
-            build(params)
-                .subscribeOn(schedulers.worker)
-                .observeOn(schedulers.ui)
-
-    }
 
     abstract class Subscribe<T, R>(private val schedulers: Schedulers) :
         UseCase<T, R, Observable<R>>() {
